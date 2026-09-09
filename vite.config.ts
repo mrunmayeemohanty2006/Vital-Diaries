18061d0186f1e2b8c2cd807aa9261847acb98e2d
Mrunmayee Mohanty <mrunmayeemohanty@Mrunmayees-MacBook-Air.local>
2026-09-09 12:19:40 +0530

    ci: add GitHub Actions workflow for automatic GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: process.env.VITE_BASE_PATH || '/',
+    base: './',
     plugins: [react(), tailwindcss()],
+
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
